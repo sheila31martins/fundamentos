36d1ce672d5ab8a836b33724a02e04d6d4b6c3cf
sheila <sheila.taylorbruno@gmail.com>
2023-04-16 16:35:46 -0300

    first commit

---
+++ inicio/desafio_area.js
@@ -0,0 +1,6 @@
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da area é " + areaCirc + "m2. ");
+console.log(Math.PI);
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+
+let c = a;
+a = b;
+b = c;
+
+
+console.log(a);
+console.log(b);
